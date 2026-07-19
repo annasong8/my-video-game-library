dcdba5ad7d902628394342a273a06ccd4c351886
GitHub Copilot <actions@github.com>
2026-07-19 10:02:53 +0000

    Adjust gallery emojis

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const galleryData = {
         games: [allGames[1], allGames[3]]
       },
       {
-        name: "🌙 Dramatic and dark",
+        name: "� Dramatic and dark",
         games: [allGames[0], allGames[2]]
       }
     ]
@@ -82,11 +82,11 @@ const galleryData = {
     title: "Based on mood",
     groups: [
       {
-        name: "🌪️ Reality- and time-bending",
+        name: "� Reality- and time-bending",
         games: [allGames[0], allGames[3]]
       },
       {
-        name: "🫶 Emotional and introspective",
+        name: "❤️‍🩹 Emotional and introspective",
         games: [allGames[2]]
       },
       {
